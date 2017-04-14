72a7c9fef2433b77d76ad2d7389a354950901ee0
siarhei.miashkou <siarhei.miashkou@leverx.com>
2017-04-14 16:33:24 +0300

    Add exception classes

---
+++ src/com/emerline/ijl01_8/exception/NegativeValueException.java
@@ -0,0 +1,13 @@
+package com.emerline.ijl01_8.exception;
+
+/**
+ * Created by dzmitry.karayedau on 14-Apr-17.
+ */
+public class NegativeValueException extends Exception {
+    public NegativeValueException() {
+    }
+
+    public NegativeValueException(String s) {
+        super(s);
+    }
+}
---
+++ src/com/emerline/ijl01_8/exception/ZeroValueException.java
@@ -0,0 +1,13 @@
+package com.emerline.ijl01_8.exception;
+
+/**
+ * Created by dzmitry.karayedau on 14-Apr-17.
+ */
+public class ZeroValueException extends Exception {
+    public ZeroValueException() {
+    }
+
+    public ZeroValueException(String s) {
+        super(s);
+    }
+}
