180f584e88fed7dee224662a10a5d4faf3b25904
siarhei.miashkou <siarhei.miashkou@leverx.com>
2017-04-14 15:11:14 +0300

    Create main for working with convert currency class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner in = new Scanner(System.in);
+        double rate;
+        double moneyCount;
+
+        try {
+            System.out.print("Input currency rate: ");
+            rate = in.nextDouble();
+            System.out.print("input money count: ");
+            moneyCount = in.nextDouble();
+            System.out.printf("Converted money = %.2f%n ", CurrencyConverter.convertCurrency("USD", rate, moneyCount));
+        } catch (InputMismatchException e) {
+            System.err.println("Wrong input data");
+        }
+
     }
 }
