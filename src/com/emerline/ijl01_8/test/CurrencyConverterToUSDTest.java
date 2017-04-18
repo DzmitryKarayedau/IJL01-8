836d3a86d0fcb6e444e78c8b7ae71552432c985c
siarhei.miashkou <siarhei.miashkou@leverx.com>
2017-04-18 17:28:59 +0300

    Rename methods and add negative situation tests

--- src/com/emerline/ijl01_8/test/CurrencyConverterToUSDTest.java
+++ src/com/emerline/ijl01_8/test/CurrencyConverterToUSDTest.java
@@ -1,10 +1,14 @@
 package com.emerline.ijl01_8.test;
 
+import com.emerline.ijl01_8.exception.NegativeValueException;
+import com.emerline.ijl01_8.exception.ZeroValueException;
 import com.emerline.ijl01_8.util.CurrencyConverterToUSD;
 
 import java.math.BigDecimal;
 
-import static junit.framework.Assert.*;
+import org.junit.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by dzmitry.karayedau on 17-Apr-17.
@@ -12,37 +16,56 @@ import static junit.framework.Assert.*;
 public class CurrencyConverterToUSDTest {
     CurrencyConverterToUSD currencyConverterToUSD = new CurrencyConverterToUSD(new BigDecimal(Math.random() + 1));
 
-    @org.junit.Before
+    public CurrencyConverterToUSDTest() throws ZeroValueException, NegativeValueException {
+    }
+
+    @Before
     public void setUp() throws Exception {
         currencyConverterToUSD = new CurrencyConverterToUSD(new BigDecimal(Math.random() + 1));
     }
 
-    @org.junit.After
+    @After
     public void tearDown() throws Exception {
         currencyConverterToUSD = null;
     }
 
-    @org.junit.Test
-    public void convertCurrency() throws Exception {
+    @Test
+    public void convertCurrency_MoneyAndRate_CountOfUsd() throws Exception {
         BigDecimal countMoney = new BigDecimal(Math.pow(Math.random(), 1 / Math.random()));
         BigDecimal rate = currencyConverterToUSD.getRate();
         assertEquals(countMoney.divide(rate, 2, BigDecimal.ROUND_HALF_UP), currencyConverterToUSD.convertCurrency(countMoney));
     }
 
-    @org.junit.Test
-    public void getCurrency() throws Exception {
+    @Test
+    public void getCurrency_USD() throws Exception {
         assertEquals("Must Be USD", "USD", currencyConverterToUSD.getCurrency());
     }
 
-    @org.junit.Test
-    public void setRate() throws Exception {
-        BigDecimal rate = new BigDecimal(Math.random() + 1);
-        currencyConverterToUSD.setRate(rate);
+    @Test
+    public void getRate_NotNull() throws Exception {
+        assertNotNull(currencyConverterToUSD.getRate());
     }
 
-    @org.junit.Test
-    public void getRate() throws Exception {
-        assertNotNull(currencyConverterToUSD.getRate());
+    @Test(expected = NegativeValueException.class)
+    public void convertCurrency_NegativeValue_NegativeValueException() throws Exception {
+        BigDecimal countMoney = new BigDecimal(Math.pow(Math.random(), 1 / Math.random())).negate();
+        currencyConverterToUSD.convertCurrency(countMoney);
+    }
+
+    @Test(expected = NegativeValueException.class)
+    public void currencyConverterToUSD_NegativeValue_NegativeValueException() throws Exception {
+        BigDecimal rate = new BigDecimal(Math.pow(Math.random(), 1 / Math.random())).negate();
+        new CurrencyConverterToUSD(rate);
+    }
+
+    @Test(expected = ZeroValueException.class)
+    public void currencyConverterToUSD_ZeroValue_ZeroValueException() throws Exception {
+        new CurrencyConverterToUSD(BigDecimal.ZERO);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void convertCurrency_Null_NullPointerException() throws Exception {
+        currencyConverterToUSD.convertCurrency(null);
     }
 
 
